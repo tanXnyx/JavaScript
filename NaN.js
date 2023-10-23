c234ceb49cbf456320432506a10a82c7882f4044
tanmay-Kumar-Swain <129646121+tanXnyx@users.noreply.github.com>
2023-10-23 21:38:34 +0530

    Add files via upload

---
+++ NaN.js
@@ -0,0 +1,12 @@
+let a = 0;
+let nan = a/a;
+let nan1 = NaN - 1;
+let nan2 = NaN + 1;
+let nan3 = NaN * 1;
+let nan4 = NaN / 1;
+console.log(nan);
+console.log(nan1);
+console.log(nan2);
+console.log(nan3);
+console.log(nan4);
+// NaN :- It is Not-a-Number

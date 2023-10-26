67fabac694c57ff5449b4ed8dc780caf39643dd7
tanmay-Kumar-Swain <129646121+tanXnyx@users.noreply.github.com>
2023-10-26 21:36:43 +0530

    Add files via upload

---
+++ NullAndUndefined.js
@@ -0,0 +1,7 @@
+let u;
+console.log(a);
+//Undefined:- A variable that has not been assigned a value is of type undefined 
+
+let n = null;
+console.log(c);
+//Null: A variable that has been assigned the value null is of type Null

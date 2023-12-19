1a53ab63992dcda48c9f73320091624c50547f25
tanmay-Kumar-Swain <129646121+tanXnyx@users.noreply.github.com>
2023-12-19 23:12:09 +0530

    Add files via upload

---
+++ prompt.js
@@ -0,0 +1,7 @@
+prompt("Enter your name: ");
+prompt("Enter your age: ");
+prompt("Enter your roll: ");
+prompt("Enter your address: ");
+
+let firstname = prompt("Enter your name: ");
+console.log(firstname)

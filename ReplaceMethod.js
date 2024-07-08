7c195aada4aafc766970bf3c22166213015dc137
tanmay-Kumar-Swain <129646121+tanXnyx@users.noreply.github.com>
2024-07-08 23:20:00 +0530

    Update ReplaceMethod.js

--- ReplaceMethod.js
+++ ReplaceMethod.js
@@ -1,5 +1,5 @@
 let text = "Visit Microsoft!";
-let result = text.replace("Microsoft", "Subhajyoti");
+let result = text.replace("Microsoft", "tan");
 
 let text1 = "Mr Blue has a blue house and a blue car";
 let result1 = text.replace(/blue/g, "red");
